b3b5d7c357fdb827c9e6e7c95239434b96a48082
marellapudihimamsu <38615128+marellapudihimamsu@users.noreply.github.com>
2020-01-23 17:50:58 +0530

    Update stockprice.py

--- stockprice.py
+++ stockprice.py
@@ -1,9 +1,3 @@
-"""import requests
-inp=input('input word')
-r = requests.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20191224T085756Z.5b8467f1b488ef86.d781c9b6fbae705b4d60ab6009c695c940f73da2&text=%22'+inp+'%22&lang=en-hi')
-z=r.json()
-ans=str(z.get('text')[0])
-print(ans[1:len(ans)-1])"""
 
 import requests 
 from bs4 import BeautifulSoup 
